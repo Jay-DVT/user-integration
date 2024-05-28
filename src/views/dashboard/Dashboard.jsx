3ea6e4839418db5a9d03d23636fd547c484b1068
DJay <diegovt.arbt@gmail.com>
2024-05-27 19:32:12 -0600

    Regsitration and user display

--- src/views/dashboard/Dashboard.jsx
+++ src/views/dashboard/Dashboard.jsx
@@ -1,26 +1,27 @@
-import { useEffect, useState } from 'react';
-import Card from './components/Card';
+import { useEffect, useState } from "react";
+import Card from "./components/Card";
 
 const Dashboard = () => {
-  const [users, setUsers] = useState([]);
-  const fetchUsers = async () => {
-    const response = await fetch('http://localhost:3000/users');
-    const data = await response.json();
-    setUsers(data);
-    console.log(data);
-  };
-  useEffect(() => {
-    fetchUsers();
-  }, []);
-  return (
-    <div>
-      {users.map((user) => (
-        <div key={user.id}>
-          <Card user={user} />
-        </div>
-      ))}
-    </div>
-  );
+	const [users, setUsers] = useState([]);
+	const fetchUsers = async () => {
+		const response = await fetch("http://localhost:3000/users");
+		const data = await response.json();
+		setUsers(data);
+	};
+	useEffect(() => {
+		fetchUsers();
+	}, []);
+	return (
+		<section className="flex justify-center m-5 ">
+			<div className='w-fit flex flex-col gap-3'>
+				{users.map((user) => (
+					<div key={user.id}>
+						<Card user={user} />
+					</div>
+				))}
+			</div>
+		</section>
+	);
 };
 
 export default Dashboard;
--- src/views/dashboard/components/Card.jsx
+++ src/views/dashboard/components/Card.jsx
@@ -1,18 +1,13 @@
 /* eslint-disable react/prop-types */
 
 const Card = ({ user }) => {
+
+  const handleClick = () => {
+    console.log("User: ", user.id);
+  }
+
   return (
-    <div
-      style={{
-        width: '230px',
-        height: '60px',
-        border: 'solid black 2px',
-        borderRadius: '5px',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
+    <div className="border-2 border-black w-full p-2 rounded-md text-center" onClick={handleClick}>
       <div>{user.name}{user.email}</div>
     </div>
   );
